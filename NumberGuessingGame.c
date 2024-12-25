3919ca188178ef75d3e9f9bcccba10646fb0b608
Sohan Rout <sohanrout84@gmail.com>
2024-12-25 18:52:46 +0530

    commit

---
+++ NumberGuessingGame.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int randomNumber, guessNumber;
+    int numberOfGuesses = 0;
+    srand(time(NULL));
+
+    printf("-------------------------------------\n");
+    printf("Welcome to the Number Guessing Game.\n");
+    printf("-------------------------------------\n");
+    randomNumber = rand() % 100 + 1;
+
+    do {
+        printf("\nplease input a number between 1 to 100 : ");
+        scanf("%d", &guessNumber);
+        printf("-------------------------------------\n");
+        numberOfGuesses++;
+
+        if(guessNumber > randomNumber){
+            printf("\tThe number is too high.\n");
+        }  
+        else if(guessNumber < randomNumber){
+            printf("\tThe number is too low.\n");
+        }
+        else {
+            printf("\n!!!Congratulations!!! You have guessed the number in %d guesses\n", numberOfGuesses);
+        }
+    } while (guessNumber != randomNumber);
+
+    printf("\n------Thanks for playing!------");
+    printf("\n----Developed By SOHAN ROUT----");
+
+    return 0;
+}
